bd988cbf5035220f38a15be273dba30c5a2ed9dd
Ashley Fletcher <ashley.fletcher76@yahoo.com>
2025-12-14 12:28:49 +0100

    lesson completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BiArchive } from "react-icons/bi";
+import Search from "./components/Search"
+
+function App() {
+  return (
+    <div className="App container mx-atuo mt-3 font-thin">
+      <h1 className="text-5xl">
+        <BiArchive className="inline-block text-red-500 align-top" />
+        Your Appointments
+      </h1>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
+
